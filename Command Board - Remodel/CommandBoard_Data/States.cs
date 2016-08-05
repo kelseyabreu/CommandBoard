a5588ab8d28ca206f984edcba1767b47f53634d3
Kelsey Abreu <kelsey@productivityapex.com>
2016-08-05 12:30:06 -0400

    No more circular dependencies!!!

---
+++ Command Board - Remodel/CommandBoard_Data/States.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Runtime.Serialization;
+
+namespace CommandBoard_Data{
+
+    [DataContract]
+    public class States
+    {
+        [DataMember]
+        public int[][] rotation { set; get; }
+
+        [DataMember]
+        public int[][] types { set; get; }
+
+        [DataMember]
+        public int[][] lvls { set; get; }
+
+        [DataMember]
+        public List<Location>[] owned { set; get; }
+
+        [DataMember]
+        public Rectangle[][] rectangleGrid { set; get; }
+
+        [DataMember]
+        public Rectangle[][] circleGrid { set; get; }
+
+        [DataMember]
+        public int[][] values { set; get; }
+
+        [DataMember]
+        public Size gridSize { set; get; }
+
+        [DataMember]
+        public Color[][] rectColor { set; get; }
+
+        [DataMember]
+        public Color[][] shapeColor { set; get; }
+
+        [DataMember]
+        public Color[][] innerRectColor { set; get; }
+
+        [DataMember]
+        public Color[][] innerShapeColor { set; get; }
+
+        [DataMember]
+        public int numOfPlayers { set; get; }
+
+        [DataMember]
+        public int totalValue { set; get; }
+
+
+    }
+
+}
